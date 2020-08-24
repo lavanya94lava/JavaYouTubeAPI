a24d3b8c098bb74c0a3cb83a3276be2ebcaf3bcf
lavanya94lava <singhlavanya94@gmail.com>
2020-08-24 15:55:36 +0530

    add POST Comment API

--- src/main/java/controllers/YouTubeController.java
+++ src/main/java/controllers/YouTubeController.java
@@ -1,22 +1,24 @@
 package controllers;
 
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.CommentThreadListResponse;
+import com.google.api.services.youtube.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import services.YouTubeService;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 
 @RestController
 public class YouTubeController {
     private static final String DEVELOPER_KEY = "AIzaSyBZW-9OYsCRSfMDZBQGErRnVh9tRaSNn1U";
     @Autowired
     YouTubeService youTubeService;
+
     @RequestMapping(value = "/getComments")
-    public CommentThreadListResponse getComments() throws GeneralSecurityException, IOException {
+    public CommentThreadListResponse getComments() throws Exception {
         YouTube youtubeService = youTubeService.getService();
         // Define and execute the API request
         YouTube.CommentThreads.List request = youtubeService.commentThreads()
@@ -26,4 +28,38 @@ public class YouTubeController {
                 .execute();
         return response;
     }
+
+    @RequestMapping(value = "/postComment", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public CommentThread postComment(@RequestBody JSONObject jsonObject) throws Exception {
+        String videoId = (String)jsonObject.get("video_id");
+
+        // Create a comment snippet with text.
+        CommentSnippet commentSnippet = new CommentSnippet();
+        commentSnippet.setTextOriginal("hi");
+
+
+        // Create a top-level comment with snippet.
+        Comment topLevelComment = new Comment();
+        topLevelComment.setSnippet(commentSnippet);
+
+        // Create a comment thread snippet with channelId and top-level
+        // comment.
+        CommentThreadSnippet commentThreadSnippet = new CommentThreadSnippet();
+        commentThreadSnippet.setVideoId(videoId);
+        commentThreadSnippet.setTopLevelComment(topLevelComment);
+
+        // Create a comment thread with snippet.
+
+        CommentThread commentThread = new CommentThread();
+        commentThread.setSnippet(commentThreadSnippet);
+
+
+        // Create a comment with snippet.
+        CommentThread videoCommentInsert = youTubeService.getService().commentThreads().insert("snippet",commentThread).execute();
+        return  videoCommentInsert;
+
+    }
+
+    @RequestMapping(value = "/replyToComment", method = RequestMethod.POST)
+    public 
 }
--- src/main/java/services/YouTubeService.java
+++ src/main/java/services/YouTubeService.java
@@ -1,26 +1,48 @@
 package services;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
 
 
 @Service
 public class YouTubeService {
 
-    private static final String APPLICATION_NAME = "API code samples";
+    private static final String CLIENT_SECRETS =    "com/example/JavaAPIYouTube/client_secrets.json";
+    private static final String APPLICATION_NAME = "Java API code";
+    private static final Collection<String> SCOPES =
+            Arrays.asList("https://www.googleapis.com/auth/youtube.force-ssl");
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    public YouTube getService() throws GeneralSecurityException, IOException {
+    public YouTube getService() throws Exception {
         final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
+        Credential credential = authorize(httpTransport);
+        return new YouTube.Builder(httpTransport, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME)
                 .build();
+    }
+
+    public Credential authorize(final NetHttpTransport httpTransport) throws Exception {
+        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("client_secrets.json");
+        System.out.println("innnnnnnnnn "+ new ClassPathResource(
+                "client_secrets.json").getInputStream());
+        GoogleClientSecrets googleClientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader         (in));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY,        googleClientSecrets, SCOPES).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize           ("user");
+        return  credential;
     }
 }
